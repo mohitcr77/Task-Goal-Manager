54efdbecd1f88adca89d2f0a9ec534b175b584a1
mohitcr77 <mohitcr77@gmail.com>
2023-03-06 14:31:27 +0530

    4th commit

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import DetailsScreen from './screens/DetailsScreen';
-import HomeScreen from './screens/HomeScreen';
+import DetailsScreen from './Screens/DetailsScreen';
+import HomeScreen from './Screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
